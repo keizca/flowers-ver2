4065e55c3078446d8f8f34c1dfb215bb256764cc
keizca <bckupacc999@gmail.com>
2025-02-14 19:20:21 +0700

    add music

--- js/index.js
+++ js/index.js
@@ -13,3 +13,21 @@ textElements.forEach((element) => {
   const randomDelay = Math.random() * 3; // Menghasilkan delay acak antara 0 hingga 3 detik
   element.style.animationDelay = `${randomDelay}s`;
 });
+
+window.addEventListener('DOMContentLoaded', () => {
+  const audio = new Audio('./audio/hereWithMe.mp3');
+  audio.volume = 0.5; // Atur volume
+  audio.loop = true;  // Putar berulang
+  audio.autoplay = true;
+  audio.muted = false;
+  
+  audio.play().catch(e => {
+    console.warn('Autoplay dicegah oleh browser. Klik dulu halaman untuk memulai musik.');
+  });
+
+  // Auto Unmute setelah interaksi
+  window.addEventListener('click', () => {
+    audio.muted = false;
+    audio.play();
+  });
+});
